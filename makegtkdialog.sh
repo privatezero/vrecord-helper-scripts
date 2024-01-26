80cc034f4aeab5a8c7763cea90ef4082d441a118
Andrew Weaver <theandrewjw@gmail.com>
2024-01-26 15:40:32 -0800

    Update makegtkdialog.sh

--- makegtkdialog.sh
+++ makegtkdialog.sh
@@ -1,1 +1,7 @@
 # Helps build vrecord dependency gtkdialog 
+wget -O 0.8.4d.tar.gz https://github.com/puppylinux-woof-CE/gtkdialog/archive/0.8.4d.tar.gz && \
+tar -xvzf 0.8.4d.tar.gz && \
+cd gtkdialog-0.8.4d && \
+./autogen.sh && \
+make
+sudo make install
