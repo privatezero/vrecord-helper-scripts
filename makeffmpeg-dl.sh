1d6bb162da63dcd44fa6c4c3c53a7c7d06243f59
Andrew Weaver <theandrewjw@gmail.com>
2024-01-29 10:20:26 -0800

    Update makeffmpeg-dl.sh

--- makeffmpeg-dl.sh
+++ makeffmpeg-dl.sh
@@ -1,20 +1,11 @@
-# Need to have lasest version of Blackmagic Desktop video installed - currently https://www.blackmagicdesign.com/support/download/f3991269fb49440d9049fd5c41f44679/Linux
-
-# For DV device use will need the following libraries installed prior to ffmpeg-dl compilation:
-#    sudo apt-get install libiec61883-dev && \
-#    sudo apt-get install libraw1394-dev &&\
-#    sudo apt-get install libavc1394-dev && \
-#    sudo apt-get install libavc1394-tools
-
-
-wget -O ffmpeg-5.1.3.tar.bz2 https://ffmpeg.org/releases/ffmpeg-5.1.3.tar.bz2 && \
-tar xjvf ffmpeg-5.1.3.tar.bz2 && \
+wget -O ffmpeg-5.1.4.tar.bz2 https://ffmpeg.org/releases/ffmpeg-5.1.4.tar.bz2 && \
+tar xjvf ffmpeg-5.1.4.tar.bz2 && \
 wget -O v10.11.2.tar.gz https://github.com/amiaopensource/SoyDecklink/archive/v10.11.2.tar.gz && \
 tar -xvzf v10.11.2.tar.gz && \
-cd ffmpeg-5.1.3 && \
+cd ffmpeg-5.1.4 && \
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure \
-  --prefix="$HOME/ffmpeg_build" \
   --pkg-config-flags="--static" \
+  --progs-suffix='-dl' \
   --extra-cflags="-I$HOME/ffmpeg_build/include" \
   --extra-ldflags=-L../SoyDecklink-10.11.2/DecklinkSdk/Linux/include/\
   --extra-ldflags="-L$HOME/ffmpeg_build/lib" \
